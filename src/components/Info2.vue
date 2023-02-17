ffb26e98fa897a3992fc5f250c38c723f69b5518
ErickBastoCen <erick.19070005@itsmotul.edu.mx>
2023-02-17 11:10:36 -0600

    Modificación de componentes

---
+++ src/components/Info2.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="blue">{{ msg }}</h1>
+    <h3>
+      
+    <a href="http://isclab.com.mx/" target="_blank" rel="noopener"> Contacto: contacto@isclab.com.mx </a>.
+
+    </h3>
+  </div>
+</template>
--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -9,11 +9,14 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="blue">{{ msg }}</h1>
     <h3>
       Desarrollamos aplicaciones con fines de práctica, investigación y responsabilidad social.
       Buscamos el desarrollo de nuestros estudiantes a través de la creación de proyectos que sean útiles a la sociedad.
+
+      <br />
       ¿Tienes un proyecto en mente? Compártelo con nosotros y buscaremos la manera de ayudarte.
+      <br />
     </h3>
   </div>
 </template>
