72c673621516445ed6aa9549c55057d0baec10d1
ErickBastoCen <erick.19070005@itsmotul.edu.mx>
2023-02-21 19:33:19 -0600

    Solución de las imágenes

--- src/components/Info6.vue
+++ src/components/Info6.vue
@@ -12,11 +12,8 @@ import Estructura2 from './Estructura2.vue'
     
   </Estructura2>
   <CENTER>
-    <img alt="imagen" class="logo" src="./media/ideathon.PNG" />
-    <img alt="imagen" class="logo" src="./media/imagen (1).jpg" />
-
-    <img alt="imagen" class="logo" src="./media/imagen (2).jpg" />
-
-    <img alt="imagen" class="logo" src="./media/imagen (3).jpg" />
+    <img alt="Logo" class="logo" src="https://scontent.cdninstagram.com/v/t39.30808-6/317558978_572468431550425_4725680847336286778_n.jpg?stp=dst-jpg_e35_p640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=101&_nc_ohc=uQbmBPceGjQAX_Z8Rf9&edm=AP_V10EAAAAA&ccb=7-5&ig_cache_key=Mjk4Mjk2MjAxMzEwNjAzNDI2Ng%3D%3D.2-ccb7-5&oh=00_AfDnnIv9ylxr-PU6drYCsOutc-bH3TeKfsL2SAVnuQfcUA&oe=63FA87AB&_nc_sid=4f375e" />
+    <img alt="Logo" class="logo" src="https://scontent.cdninstagram.com/v/t39.30808-6/315191144_555161756614426_1640998221380803573_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=scontent.cdninstagram.com&_nc_cat=110&_nc_ohc=2xuWJX6a--MAX_xtTrY&edm=AP_V10EAAAAA&ccb=7-5&ig_cache_key=Mjk2ODA2NTY5NTU4MjcyMjIwOQ%3D%3D.2-ccb7-5&oh=00_AfA7nbMxUGCcLqPykYl920WMVV6UPtO7YorTZZG9hmz2rw&oe=63FAC2DA&_nc_sid=4f375e" />
+    <img alt="Logo" class="logo" src="https://instagram.fcps2-1.fna.fbcdn.net/v/t39.30808-6/315546202_559387032858565_7325351545707525679_n.jpg?stp=dst-jpg_e35_s640x640_sh0.08&_nc_ht=instagram.fcps2-1.fna.fbcdn.net&_nc_cat=101&_nc_ohc=n7hyIMMjTt0AX_ElO3g&edm=AP_V10EAAAAA&ccb=7-5&ig_cache_key=Mjk3MTQ3NDI0NzI3Nzk4MjU1MQ%3D%3D.2-ccb7-5&oh=00_AfBLlxEvNSBVnseJyOOR1bUJD5a2BbFFYY1Xlag5UNpBCw&oe=63FAA4A7&_nc_sid=4f375e" />
   </CENTER>
 </template>
